0dc2dd7feb279784f6ffe43dc936569ee54c4a28
yuya17 <yuya.noza17@gmail.com>
2018-08-17 15:18:47 +0900

    Englishパッケージを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          //child: Text('Hello World'),
+          child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
